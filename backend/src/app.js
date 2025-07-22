fabe2898abdcbaeac76ba62906e8de190557306b
Marcel <focused.cagey.81@icloud.com>
2025-07-22 18:16:49 +0000

    serve static frontend files from public/ directory

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,15 @@
 import express from 'express';
 import questionsRouter from '../routes/questions.js';
 import answersRouter   from '../routes/answers.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
+
+//serve frontend assets from backend/public
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(espress.static(path.join(__dirname, '../public')));
 
 // make sure to parse JSON bodies
 app.use(express.json());
